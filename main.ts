6dd36325273a573cefc24ae2abbf77e53ddadedd
sufyan-mirza <160243704+sufyan-mirza@users.noreply.github.com>
2024-06-26 21:21:56 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,62 @@
+// CURRENCY CONVERTER
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+let CONVERTER={
+    PKR:{
+        USD:0.0036,
+        EURO:0.0033,
+        PKR:1
+        
+    },
+    EURO:{
+        PKR:301.24,
+        USD:1.08,
+        EURO:1
+    },
+    USD:{
+        PKR:278.12,
+        EURO:0.92,
+        USD:1
+    }
+}
+
+let currency:{
+    
+        from:"PKR"|"EURO"|"USD",
+        to:"PKR"|"EURO"|"USD",
+    
+        amount:number
+}=await inquirer.prompt([
+    {
+        type:'list',
+        name:'from',
+        message:'enter the currency you want to convert',
+        choices:["PKR","EURO","USD"]
+
+    },
+    {
+        type:'list',
+        name:'to',
+        message:'enter the currency in which you want to convert',
+        choices:["PKR","EURO","USD"]
+    },
+    {
+        type:'number',
+        name:'amount',
+        message:'enter the amount you want  to convert'
+    }
+    
+])
+const {from,to,amount}=currency
+
+// we use if else statement to convert the currency
+if(from&&to&&amount){
+    let answer=CONVERTER[from][to]*amount
+    console.log(`you have converted ${amount} ${from} to ${to}`)
+
+}
+else{
+    console.log("invalid error")
+}
